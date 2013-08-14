b058a60d6f5831ae7976c7fb5d03228a87bb0de4
carlos arboleda <carlosarboleda@MacBook-Pro-de-carlos-arboleda.local>
2013-08-14 10:38:54 -0500

    Cracion Proyecto Ruby On Rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Miproyecone::Application.config.secret_token = 'e5add74f53edbcabc13af837c050e5805d3de0923f5d7f3ed60b51c1518963a6dff6dc6cead37994a0f68c0616899b31c9243254b5b46c098a9c1a2ac7c2b8d5'
